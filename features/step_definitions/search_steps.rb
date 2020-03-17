8bb1c0716ac0bff4884fe05eab3c2eb64e08fcf4
Lucas Raphael <lucasraphaell@gmail.com>
2020-03-17 10:54:37 -0300

    Cria steps dos cenários de busca por professor.

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,60 +1,45 @@
-  When("i click on By Teacher item on the navigation bar") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i must visualize the courses by teacher's page") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("when i click on teacher {string} details button") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i must visualize the course list page from {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When("i click on {string} course") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i must visualize the course page") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i can compare the list price with the installment price") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When("when i type {string} on the filter input") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When("when i click on teacher {string} details button") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i can validate if the amount of courses describe on header's page matches with amount of course listed") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i must visualize the course page details") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When("i click on Buy Now button") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i need to sign in with my user data:") do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("click on login button") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("i must visualize de cart page") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+When("i click on By Teacher item on the navigation bar") do
+  @navigation_bar_component.search_by_teacher_btn().click
+end
+
+# Search by Teachers List
+Then("i must visualize the courses by teacher's page") do  
+  expect(@course_by_teacher_page.page_header().text).to eql "Cursos por professor"
+  @course_by_teacher_page.scroll_to_teachers_list()
+end
+
+And("when i click on teacher {string} details button") do |teacher|
+  @course_by_teacher_page.teacher_details_btn(teacher).click
+end
+
+# Search by Filter
+When("when i type {string} on the filter input") do |teacher|
+  @course_by_teacher_page.scroll_to_filter()
+  @course_by_teacher_page.filter_by_teacher(teacher)
+end
+
+Then("i must visualize the course list page from {string}") do |teacher|
+  expect(@teacher_page.page_header().text).to eql teacher
+end
+
+# Validate amount of courses on Teacher's Page
+Then("i can validate if the amount of courses describe on header's page matches with amount of course listed") do
+  puts "Amount of Courses description: #{@teacher_page.get_amount_courses}"
+  puts "Amount of Courses listed on page: #{@teacher_page.get_amount_courses_list}"
+  expect(@teacher_page.get_amount_courses).to eql @teacher_page.get_amount_courses_list()
+end
+
+When("i click on {string} course") do |course|
+  @course = course
+  @teacher_page.course_details_btn(course).click
+end
+
+Then("i must visualize the course page") do
+  expect(@course_page.page_header().text).to eql @course
+end
+
+And("i can compare the list price with the installment price") do
+  puts "Retail Price: R$#{@course_page.get_retail_price}"
+  puts "Installment Price: R$#{@course_page.get_installment_price}"
+  expect(@course_page.get_retail_price).to eql @course_page.get_installment_price()
+end
