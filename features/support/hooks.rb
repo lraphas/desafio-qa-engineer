c558e546742059b2463a6d6a27cbdca8d444e8c3
Lucas Raphael <lucasraphaell@gmail.com>
2020-03-17 11:12:27 -0300

    Configura geração de screenshots em hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,12 @@
 Before do
-    page.current_window.resize_to(1366, 768)
-    @navigation_bar_component = NavigationBarComponent.new
-    @course_by_teacher_page = CourseByTeacherPage.new
-    @teacher_page = TeacherPage.new
-    @course_page = CoursePage.new
+  page.current_window.resize_to(1366, 768)
+  @navigation_bar_component = NavigationBarComponent.new
+  @course_by_teacher_page = CourseByTeacherPage.new
+  @teacher_page = TeacherPage.new
+  @course_page = CoursePage.new
+end
+  
+After do |scenario|
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
 end
