f9329979a013f09d310e57bbc45601473b825cae
Lucas Raphael <lucasraphaell@gmail.com>
2020-03-17 11:13:46 -0300

    Redefine configuração do Capybara web-driver. Atualiza cucumber.yaml

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,14 +2,31 @@ require "capybara"
 require "capybara/cucumber"
 require "selenium-webdriver"
 
-@browser = ENV['BROWSER']
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
-Capybara.configure do |config|
-    config.default_driver = :selenium if @browser.eql?('firefox')
-    config.default_driver = :selenium_chrome if @browser.eql?('chrome')
-    config.default_driver = :selenium_chrome_headless if @browser.eql?('headless')
-    config.app_host = 'https://www.estrategiaconcursos.com.br'
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+  @driver = :selenium_chrome_headless
+else
+  puts "Invalid browser"
 end
 
-Capybara.ignore_hidden_elements = false
-Capybara.default_max_wait_time = 10
+Capybara.configure do |config|
+  config.default_driver = @driver
+  config.app_host = CONFIG["url"]
+  config.default_max_wait_time = 10
+  config.ignore_hidden_elements = false
+end
