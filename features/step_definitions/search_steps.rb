26271a304487b3d88d9e2be5964b9a774c574616
Lucas Raphael <lucasraphaell@gmail.com>
2020-03-17 10:42:08 -0300

    Adiciona home_steps e search_steps em step_definitions/

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,64 @@
+Given("i am on the home page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("i click on By Teacher item on the navigation bar") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i must visualize the courses by teacher's page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("when i click on teacher {string} details button") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i must visualize the course list page from {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("i click on {string} course") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i must visualize the course page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i can compare the list price with the installment price") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("when i type {string} on the filter input") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("when i click on teacher {string} details button") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i can validate if the amount of courses describe on header's page matches with amount of course listed") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i must visualize the course page details") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("i click on Buy Now button") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i need to sign in with my user data:") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("click on login button") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("i must visualize de cart page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
